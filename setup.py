2fcb398b6d239c824ef62c69777ff0d50b8e7f51
ladvien <honeysucklehardware@gmail.com>
2020-05-26 18:18:45 -0500

    Working on script

--- setup.py
+++ setup.py
@@ -14,29 +14,49 @@ os.system('pip install esptool pyserial glob')
 os.system(f'wget https://micropython.org/resources/firmware/{firmware_file_name}')
 os.system('wget https://raw.githubusercontent.com/Ladvien/ramps_controller/master/ramps_controller/serial_util.py')
 
-# Grab some code I wrote (or stole?) for a different project.
+# Grab some code I wrote (or stole? I cant remember) for a different project.
 from serial_util import *
 
 # Get the port from the user.
 print('########################################################')
-print('# Welcome to Ladvien\'s ESP32 Micropython setup script #')
-print('# First, select the port your ESP32 is on.             #')
+print("# Welcome to Ladvien's ESP32 Micropython setup script  #")
+print('# How can it help?                                     #')
+print('#                                                      #')
+print('# 1: CONNECT to Micropython on ESP32                   #')
+print('# 2: INSTALL to Micropython on ESP32                   #')
+print('#                                                      #')
+print('########################################################')
+use_case = input(': ')
+print()
+print()
+print('########################################################')
+print('# Select your port:                                    #')
 print('########################################################')
 port = get_write_port()
 print()
 print()
 
-# Flash the ESP32
-input('About Erasing EVERYTHING on the ESP32. Hit a key if you accept.')
-os.system(f'esptool.py --chip esp32 -p {port} erase_flash')
+if use_case == '2':
+    # Flash the ESP32
+    input('About Erasing EVERYTHING on the ESP32. Hit a key if you accept.')
+    os.system(f'esptool.py --chip esp32 -p {port} erase_flash')
 
-# Install Micropython
-print()
-print()
-print('Installing Micropython!')
-os.system(f'esptool.py --chip esp32 -p {port} write_flash -z 0x1000 {firmware_file_name}')
+    # Install Micropython
+    print()
+    print()
+    print('Installing Micropython!')
+    print('Note: If you have problems connecting, try holding the BOOT button connected.')
+    os.system(f'esptool.py --chip esp32 -p {port} write_flash -z 0x1000 {firmware_file_name}')
+elif use_case == '1':
+    # Test
+    print()
+    print('Testing the device...')
+    os.system(f'screen {port} {115200}')
+    print()
 
-# Debrief
-print()
-print('Huzzah! We are all done!')
-print()
+    # Debrief
+    print()
+    print('Huzzah! We are all done!')
+    print()
+else:
+    print('Not a valid selection. Run once more to try again.')
