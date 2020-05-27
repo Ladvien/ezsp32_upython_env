de9488870f3064302b8f49eac5d8cae1905879b5
ladvien <honeysucklehardware@gmail.com>
2020-05-26 19:16:36 -0500

    Working on mounting via mpy-utils

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 ##############
 # Parameters
@@ -10,7 +11,7 @@ firmware_file_name          = 'tinypico-idf4-20200526-unstable-v1.12-464-gcae77d
 #################
 
 # Download tools
-os.system('pip install esptool pyserial glob')
+os.system('pip install esptool pyserial mpy-utils --quiet')
 if not os.path.exists(firmware_file_name):
     os.system(f'wget https://micropython.org/resources/firmware/{firmware_file_name}')
 if not os.path.exists('serial_util.py'):
@@ -24,9 +25,9 @@ print('########################################################')
 print("# Welcome to Ladvien's ESP32 Micropython setup script  #")
 print('# How can it help?                                     #')
 print('#                                                      #')
-print('# 1: CONNECT to Micropython on ESP32                   #')
-print('# 2: INSTALL to Micropython on ESP32                   #')
-print('#                                                      #')
+print('# 1: CONNECT                                           #')
+print('# 2: INSTALL                                           #')
+print('# 3: MOUNT                                                     #')
 print('########################################################')
 use_case = input(': ')
 print()
@@ -44,15 +45,16 @@ if use_case == '2':
     os.system(f'esptool.py --chip esp32 -p {port} erase_flash')
 
     # Install Micropython
-    print()
-    print()
     print('Installing Micropython!')
     print('Note: If you have problems connecting, try holding the BOOT button connected.')
     os.system(f'esptool.py --chip esp32 -p {port} write_flash -z 0x1000 {firmware_file_name}')
 elif use_case == '1':
-    # Test
+    # Connect
     print()
-    print('Testing the device...')
+    print('Connecting to the device using "screen" terminal')
+    print('To exit hit "CTRL+A" then "CTRL+D"')
+    print('')
+    input('Hit any key to connect then reset your ESP32....')
     os.system(f'screen {port} {115200}')
     print()
 
@@ -60,5 +62,11 @@ elif use_case == '1':
     print()
     print('Huzzah! We are all done!')
     print()
+elif use_case == '3':
+    # Connect
+    print()
+    print('Mounting the ESP32 filesystem to "./esp32"')
+    print('Keep this window open while manipulating the filesystem.')
+    print(f'mpy-fuse --port {port} ')
 else:
     print('Not a valid selection. Run once more to try again.')
